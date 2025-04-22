727f9ea3b1d4f5f6e4fe1c9d0cffad033a683dce
Alesio <folinialesio@gmail.com>
2025-04-22 09:21:15 -0300

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,20 +1,23 @@
-const CACHE_NAME = 'expense-splitter-v1';
-const urlsToCache = [
-  './',
-  './index.html',
-  './style.css',
-  './app.js',
-  './manifest.json',
-  './icon-192.png',
-  './icon-512.png'
-];
 self.addEventListener('install', event => {
   event.waitUntil(
-    caches.open(CACHE_NAME).then(cache => cache.addAll(urlsToCache))
+    caches.open('v1').then(cache => {
+      return cache.addAll([
+        './',
+        './index.html',
+        './style.css',
+        './app.js',
+        './manifest.json',
+        './icon-192.png',
+        './icon-512.png'
+      ]);
+    })
   );
 });
+
 self.addEventListener('fetch', event => {
   event.respondWith(
-    caches.match(event.request).then(response => response || fetch(event.request))
+    caches.match(event.request).then(response => {
+      return response || fetch(event.request);
+    })
   );
 });
